b44e4f2d939fb52376740e8570bbe3734e549064
Tycho Tatitscheff <tycho.tatitscheff@gmail.com>
2013-08-26 22:55:53 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Seaborgium::Application.config.secret_key_base = 'f1ce6b810d52fe1477488099fbd15ece8cf0742c9d5a873a8b57882ff2db7cabcdc9367848f3ad197b1dcf3e2b3b749a2bd0e2d2e3fe570c9d8210cf8b5713bf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Seaborgium::Application.config.session_store :cookie_store, key: '_Seaborgium_session'
