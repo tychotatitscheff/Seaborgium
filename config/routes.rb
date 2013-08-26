c3448ea7633580d5c355b66a9a388c002c8e2055
Tycho Tatitscheff <tycho.tatitscheff@gmail.com>
2013-08-26 23:21:11 +0200

    Test de base

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Seaborgium::Application.routes.draw do
+  get "say/file"
+  get "say/hello"
+  get "say/goodbye"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
